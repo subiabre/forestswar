c41d0da5037f52651edfcf2c752a25af8c6e0474
Facu <facuwebdev@gmail.com>
2020-01-14 17:32:40 +0100

    Make country fetching error recursive

--- src/service/glad.js
+++ src/service/glad.js
@@ -97,35 +97,53 @@ class GLAD
     /**
      * Fetch alerts for a given country in a given period
      * @param {string} country Country ISO3 code
-     * @param {string} period A period of two dates
-     * @returns {number} Number of square kilometers deforestated
+     * @param {string} period A period of two date
+     * @returns {object} The response object 
      */
-    async getAlertsCountry(country, period)
+    async fetchCountry(country, period)
     {
         return new Promise((resolve, reject) => {
             let api = this.api + '/admin/' + country + period;
 
-            this.http.get(api, async (res) => {
+            this.http.get(api, (res) => {
                 let alerts = '';
 
                 res.on('data', (data) => {
                     alerts += data;
                 });
 
-                res.on('end', async () => {
+                res.on('end', () => {
                     alerts = JSON.parse(alerts);
                     
-                    if (alerts.errors) {
-                        alerts = { data: { attributes: { value: 0 } } };
-                    }
-
-                    let area = this.alertsArea(alerts);
-                    resolve(area);
+                    resolve(alerts);
                 });
-            }).on('error', () => {
-                this.getAlertsCountry(country, period);
+            }).on('error', (error) => {
+                reject(error);
             });
         });
+    }
+
+    /**
+     * Fetch alerts for a given country in a given period, fail-safe
+     * @param {string} country Country ISO3 code
+     * @param {string} period A period of two dates
+     * @returns {number} Number of square kilometers deforestated
+     */
+    async getAlertsCountry(country, period)
+    {
+        let alerts = await this.fetchCountry(country, period);
+
+        if (alerts.errors) {
+            if (alerts.errors[0].status == 500) {
+                alerts = { data: { attributes: { value: 0 }}};
+            }
+
+            else {
+                alerts = await this.fetchCountry(country, period);
+            }
+        }
+
+        return this.alertsArea(alerts);
     }
 
     /**
