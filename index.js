d640dc4013d6dfef504d5a902a9c1197c65b3b86
Facu <subiabrewd@gmail.com>
2020-03-14 13:11:40 +0100

    Update bot launch

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ var express = require('express'),
     bot = new Deforestation();
 
 app.get('/status', (req, res) => {
-    res.send({
-        data: bot.getLog()
-    });
+    res.send(bot.getLog());
 });
 
 var listener = app.listen(process.env.PORT, async () => {
-    await bot.routine();
-    
     let address = 'http://localhost:' + listener.address().port;
     console.log('SERVER LISTENING AT: ' + address);
     console.log('REPORTING STATUS AT: ' + address + '/status');
+
+    await bot.routine();
 });
