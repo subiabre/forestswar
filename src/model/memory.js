9097c46e42481af5584f731cc9224c3f303d25ab
Facu <subiabrewd@gmail.com>
2020-07-15 23:09:33 +0200

    Update memory model

--- src/model/memory.js
+++ src/model/memory.js
@@ -4,32 +4,39 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var memorySchema = new Schema({
+    
+    /**
+     * @var {Date} date Date of memory generation
+     */
     date: {
         type: Date,
-        default: new Date().toString(),
+        default: new Date(),
         immutable: true
     },
 
+    /**
+     * @var {Date} gladLatest Date of latest glad alert
+     */
     gladLatest: {
         type: Date,
         required: true
     },
-
+    
+    /**
+     * @var {Number} country List index of the country
+     */
     country: {
         type: Number,
         default: 0,
         required: true,
     },
 
+    /**
+     * @var {Number} area Area of deforestation
+     */
     area: {
         type: Number,
         default: 0
-    },
-
-    countryDeforestatedArea: {
-        type: Number,
-        default: 0,
-        required: true
     }
 });
 
