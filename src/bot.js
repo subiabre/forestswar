376ac7f2964d7f678a2cb9133e20800f204aaedd
Facu <subiabrewd@gmail.com>
2020-07-16 11:14:35 +0200

    Remove unused aggregation

--- src/bot.js
+++ src/bot.js
@@ -209,7 +209,6 @@ class Deforestation
             // Get deforestated area in comparison to country forest area
             let ratio = gladArea * 100 / countryList.area,
                 deforestationArea = ratio * country.area / 100;
-                deforestationArea += memory.area;
             
             // Get map with deforestated area
             let map = await this.map.setCountry(country.alpha3Code);
